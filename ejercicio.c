ffb9b6603774e685c894c9b3a0f01281685ba1fb
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-22 14:52:49 -0500

    Aplicación de sizeof y promedio de notas -Mateo Mero

--- ejercicio.c
+++ ejercicio.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-float notas[] = {6.5 , 7.1, 6, 5, 8.1};
 
-int main(){
-    for (int i = 0; i < 5; i++, notas[i]){
-        printf("%.1f\n", notas[i] );
+int main() {
+    float notas[] = {6.5, 7.1, 6.0, 5.0, 6.1};
+    float suma = 0;
+    int n = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < n; i++) {
+        printf("%.1f\n", notas[i]);
+        suma = suma + notas[i];
     }
+
+    printf("El promedio es %.2f\n", (float)suma / n);
+
     return 0;
 }
