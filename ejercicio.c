945e9012e0e8b69adeb9b08b2a0d0db874bc7558
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-22 15:16:21 -0500

    Paso de parámetros x referencia -Mateo Mero

--- ejercicio.c
+++ ejercicio.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+float calcular_promedio(float *a);
 
-int main() {
-    float notas[] = {6.5, 7.1, 6.0, 5.0, 6.1};
-    float suma = 0;
-    int n = sizeof(notas) / sizeof(notas[0]);
+float notas[] = {6.5 , 7.1, 6, 5, 6.1};
 
-    for (int i = 0; i < n; i++) {
-        printf("%.1f\n", notas[i]);
-        suma = suma + notas[i];
-    }
-
-    printf("El promedio es %.2f\n", (float)suma / n);
-
+int n = sizeof (notas) / sizeof (notas[0]);
+int main(){
+    printf("\nEl promedio puntero -> %.2f", calcular_promedio(notas));
     return 0;
+}
+
+float calcular_promedio(float *a) {
+    float suma = 0;
+    for (int i = 0; i < 5; i++){
+        printf("%.1f\n", a[i] );
+        suma = suma + a[i];
+    }
+    return suma / 5;
 }
